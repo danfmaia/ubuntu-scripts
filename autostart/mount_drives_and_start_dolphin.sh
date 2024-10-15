383e03798eb5bfefb68b1bb722e75ec9b7a27af5
Dan Maia <danilofmaia@gmail.com>
2024-10-15 19:37:30 -0300

    update

--- autostart/mount_drives_and_start_dolphin.sh
+++ autostart/mount_drives_and_start_dolphin.sh
@@ -1,3 +1,3 @@
 rclone --config=/home/danfmaia/.config/rclone/rclone.conf --daemon --vfs-cache-mode=writes mount Google_Drive: ~/gdrive \
-      && rclone --config=/home/danfmaia/.config/rclone/rclone.conf --daemon --vfs-cache-mode=writes mount OneDrive: ~/OneDrive \
-      && dolphin -stylesheet ~/.config/qt5ct/qss/dolphin.css
+&& rclone --config=/home/danfmaia/.config/rclone/rclone.conf --daemon --vfs-cache-mode=writes mount OneDrive: ~/OneDrive
+# && dolphin -stylesheet ~/.config/qt5ct/qss/dolphin.css
